811258102edaf0656285abdb5fd61a7335da4618
JL Alias <jluis.alias@gmailcom>
2015-09-22 00:32:52 +0200

    I've created a getter for items by name, and I've overrided the constructor by default

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -6,7 +6,17 @@ public class GildedRose {
 
 	private static List<Item> items = null;
 
-	/**
+    public GildedRose() {
+        items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+    }
+
+    /**
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -22,7 +32,7 @@ public class GildedRose {
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
         updateQuality();
-}
+    }
 
 
 	
@@ -114,5 +124,15 @@ public class GildedRose {
             result += "\n";
         }
         return result;
+    }
+
+    public Item getItemByName(String name){
+        if(this.items == null)
+            throw new NullPointerException("GildedRose has not been initialized, call main function");
+        for(Item item : items){
+            if(name.equals(item.getName()))
+                return item;
+        }
+        return null;
     }
 }
